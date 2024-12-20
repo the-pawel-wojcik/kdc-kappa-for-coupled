41e551b907e9158ea92da7be3cdb70a70eda06f0
Paweł Wójcik <pawel.wojcik5@gmail.com>
2024-12-20 10:22:41 -0800

    Add the --show_overlaps flag to the get_points.py script

--- src/get_points.py
+++ src/get_points.py
@@ -4,6 +4,8 @@ import argparse
 import json
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
+import seaborn as sbs
 
 
 class keyvalue(argparse.Action):
@@ -59,8 +61,47 @@ def get_args():
         action=keyvalue
     )
     parser.add_argument('-v', '--verbose', default=False, action='store_true')
+    parser.add_argument('--show_overlaps', default=False, action='store_true')
     args = parser.parse_args()
     return args
+
+
+def find_matching_amplitude(ket, amplitude) -> dict[str, float | int] | None:
+    for right_amp in ket:
+        if right_amp['A'] != amplitude['A']:
+            continue
+        if right_amp['I'] != amplitude['I']:
+            continue
+        return right_amp
+    return None
+
+
+def singles_overlap(bra: list[dict], ket: list[dict]) -> float:
+    overlap: float = 0.0
+    for bra_amplitude in bra:
+        ket_amplitude = find_matching_amplitude(ket, bra_amplitude)
+        if ket_amplitude is None:
+            continue
+        overlap += bra_amplitude['amplitude'] * ket_amplitude['amplitude']
+
+    return overlap
+
+
+def show_overlaps(amplitudes):
+    for key, amps in amplitudes.items():
+        fig, ax = plt.subplots()
+        singles_matrix = np.zeros(
+            shape=(len(amps), len(amps)), dtype=np.float64
+        )
+        for bra_idx, bras in enumerate(amps):
+            bra = bras['singles']
+            for ket_idx, kets in enumerate(amps):
+                ket = kets['singles']
+                singles_matrix[bra_idx][ket_idx] = singles_overlap(bra, ket)
+
+        sbs.heatmap(singles_matrix, ax=ax, square=True)
+        ax.set_title(key)
+        plt.show()
 
 
 def main():
@@ -68,15 +109,24 @@ def main():
     pick_states = args.pick_states
 
     energies_au = {key: list() for key in pick_states}
+    amplitudes = {key: list() for key in pick_states}
+
     for file_id, fname in enumerate(args.roots):
         with open(fname, 'r') as file:
             roots = json.load(file)
             for state_name, state_number in pick_states.items():
                 root = roots[state_number]
                 energies_au[state_name].append(root['energy']['total']['au'])
+                amplitudes[state_name].append(root['converged root'])
                 if args.verbose:
                     print(f"{state_name} amplitudes: ", end="")
                     show_amps(root)
+
+    # Test that all states declared as "the same" state have reasonably alike
+    # amplitudes
+    if args.show_overlaps is True:
+        show_overlaps(amplitudes)
+
     out_pack = {
         state_name: {'energies, au': energies_au[state_name]}
         for state_name in pick_states
